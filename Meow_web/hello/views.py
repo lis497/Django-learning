0da3c628448da41a6f2220dd08f6c6b690b558cb
Linlin Sun <lis497@icloud.com>
2025-01-06 16:48:32 -0500

    1.create newyear app 2.create static folder and style.css 3.add index.html to templates 4.modify views.py

--- Meow_web/hello/views.py
+++ Meow_web/hello/views.py
@@ -7,4 +7,12 @@ def index(request):
 	return HttpResponse("Hello, <h1 style='color:orange'>Meow!</h1>")
 
 def greet(request, name):
-	return HttpResponse(f"Hello, {name}!")
+	#return HttpResponse(f"Hello, {name}!")
+	print(type(request))
+	print(request)
+	#request is a dictionary_like object, return a key
+	print(dict(request).keys())
+	return render(request, "hello/greet.html",{
+			"name": name
+		})
+
