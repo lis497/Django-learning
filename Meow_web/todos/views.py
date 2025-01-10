07458903350117d4110bb0676ed379f333d9c2d4
Linlin Sun <lis497@icloud.com>
2025-01-10 14:24:28 -0500

    add django form and setup client side validation 1

--- Meow_web/todos/views.py
+++ Meow_web/todos/views.py
@@ -1,13 +1,20 @@
+from django import forms
 from django.shortcuts import render
 
 # Create your views here.
 tasks =["foo Meow", "bar Dog", "baz Nimo"]
 
+class NewTaskForm(forms.Form):
+	task = forms.CharField(label="Add Task")
+	priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
+
 def index(request):
-	return render(request, "todos/index.html",
-		{
+	return render(request, "todos/index.html", {
 			"todos": tasks
 		})
 
 def add(request):
-	return render(request, "todos/add.html")
+	return render(request, "todos/add.html",{
+			"form":NewTaskForm()
+		})
