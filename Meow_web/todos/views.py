2147a66de3e4a85024fbba7da2afead9b6fa4578
Linlin Sun <lis497@icloud.com>
2025-01-13 14:39:01 -0500

    add http redirect to todos 1

--- Meow_web/todos/views.py
+++ Meow_web/todos/views.py
@@ -1,12 +1,14 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 tasks =["foo Meow", "bar Dog", "baz Nimo"]
 
 class NewTaskForm(forms.Form):
 	task = forms.CharField(label="Add Task")
-	priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+	priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 
 def index(request):
@@ -15,6 +17,19 @@ def index(request):
 		})
 
 def add(request):
+	if request.method == "POST":
+		form = NewTaskForm(request.POST)
+		#print(type(form),form.is_valid(),form)
+		#print(form.cleaned_data)
+		if form.is_valid():
+			task = form.cleaned_data["task"]
+			tasks.append(task)
+			return HttpResponseRedirect(reverse("todos:index"))
+		else:
+			return render(request, "todos/add.html",{
+					"form": form
+				})
+
 	return render(request, "todos/add.html",{
 			"form":NewTaskForm()
 		})
